d1231841437b30cf902f01b1e13b3d5bb92e1ef7
bmead92 <bmead92@gmail.com>
2019-10-17 22:53:36 -0400

    Update formatting of quote, author, and next button

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Quote = ({ quote }) => {
     return (
         <div className="ui message">
-            <h1 className="ui header">{quote.text}</h1>
-            <h3>{quote.author}</h3>
+            <h1 className="ui header">"{quote.text}"</h1>
+            <h3>- {quote.author}</h3>
         </div>
     )
 }
